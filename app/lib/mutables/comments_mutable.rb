8279596e0f0ad4b30c79d4fa1363c7c70f5ae26b
Patrick Lindsay <tlindsay@una.edu>
2016-12-12 11:37:26 -0600

    Switch from redis to DB

--- app/lib/mutables/comments_mutable.rb
+++ app/lib/mutables/comments_mutable.rb
@@ -1,15 +1,9 @@
 Pakyow::App.mutable :comment do
   query :for_default_post do
-    redis.lrange(:comments, 0, -1).each_with_index.map do |content, id|
-      {
-        id: id+1,
-        content: content
-      }
-    end
+    Comment.all.reverse.map(&:values)
   end
 
   action :create do |params|
-    redis.lpush(:comments, params[:content])
     Comment.create(content: params[:content])
   end
 end
